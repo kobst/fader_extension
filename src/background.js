6c0b1c7f49a7bf203486c1146088af249aca870e
Edward Han <edwrdhn@gmail.com>
2023-01-16 16:45:31 -0500

    working w no threejs, tabData structure

--- src/background.js
+++ src/background.js
@@ -1,29 +1,239 @@
-let urls = [];
 
-// listen for changes to the user's browsing history
-chrome.history.onVisited.addListener(function (historyItem) {
-  // get the current time
-  // let currentTime = Date.now();
-  console.log(historyItem.url);
-  // console.log(JSON.stringify(historyItem))
-  urls.push(historyItem.url);
-  // if the tab is already in the map, update its active time
 
-  chrome.runtime.sendMessage({ type: 'sendHistoryFromBackground', data: urls }, function (response) {
-    console.log(response.farewell);
-  })
+// a url and what time it was navigated to
+// let urlActiveEvent = {
+//     url: '',
+//     timestamp:  Date.now() 
+// }
+
+// an array of urls and the times they were activated
+// let urlHistory = [urlActiveEvent]
+
+// let tabActives = {
+//     startTime: Date.now(),
+//     endTime: Date.now(),
+// }
+
+// let tabData = {
+//     id: '',
+//     urls: urlHistory,
+//     activeTimes: tabActives
+// }
+
+// let tabs = [tabData]
+
+
+// tabData = {
+//     tabId1: {
+//       url: {
+//           url1: timestamp1,
+//           url2: timestamp2,
+//           ...
+//       },
+//       active: [timestamp1, timestamp2, ...]
+//     },
+//     tabId2: {
+//       url: {
+//           url1: timestamp1,
+//           url2: timestamp2,
+//           ...
+//       },
+//       active: [timestamp1, timestamp2, ...]
+//     },
+//     ...
+//   }
+  
+
+  ////////////////////    USING LOCAL STORAGE  TO GET THE URLS OF ALL THE TABS    //////////////////////
+
+let tabData = {}
+
+chrome.storage.local.get('tabData', function(result) {
+    console.log('get data from storage')
+    if(result.tabData){
+        tabData = result.tabData;
+    }else{
+        tabData = {};
+    }
+});
+
+// 
+
+chrome.tabs.query({}, function(tabs) {
+    for (var i = 0; i < tabs.length; i++) {
+      var tab = tabs[i];
+      tabData[tab.id] = {
+        url: {},
+        active: []
+      };
+      tabData[tab.id].url[tab.url] = Date().toString();
+    }
+    console.log('update tabData onQuery')
+    chrome.storage.local.set({tabData: tabData});
+
+  });
+
+chrome.tabs.onActivated.addListener(function(activeInfo) {
+    var tabId = activeInfo.tabId;
+    if(!tabData[tabId]){
+        tabData[tabId] = {
+            url: {},
+            active: []
+        }
+    }
+    tabData[tabId].active.push(Date().toString());
+    console.log('update tabData onActivated')
+    chrome.storage.local.set({tabData: tabData});
 
 });
 
 
+chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
+    if (changeInfo.url) {
+        if(!tabData[tabId]){
+            tabData[tabId] = {
+                url: {},
+                active: []
+            }
+        }
+        tabData[tabId].url[changeInfo.url] = Date().toString();;
+        console.log('update tabData onUpdated')
+        chrome.storage.local.set({tabData: tabData});
+    }
+});
 
-chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
-    console.log('message received in background......')
-      if (request.type === 'getHistory') {
-        // Get the user's history from the chrome.history API
-        console.log('get history');
-        console.log(urls)
-        sendResponse({ history: urls});
-      }
-    });
+chrome.tabs.onRemoved.addListener(function(tabId, removeInfo) {
+    delete tabData[tabId];
+    chrome.storage.local.set({tabData: tabData});
+});
+
+
+
+
+
+
+
+
+
+
+  ////////////////////    PREVIOUS IMPLEMENTATION    //////////////////////
+
+
+// chrome.tabs.query({}, function(tabs) {
+//     for (var i = 0; i < tabs.length; i++) {
+//       if (tabs.includes(tabs[i].id)) {
+//         continue
+//       }
+//       var row = document.createElement("tr");
+//       row.innerHTML = "<td>" + tabs[i].id + "</td>" +
+//                       "<td>" + tabs[i].url + "</td>" +
+//                       "<td>" + tabs[i].lastAccessed + "</td>";
+//       tabList.appendChild(row);
+//       tabs.push(tabs[i].id)
+//     }
+//   });
+
+
+// listen for changes to the user's browsing history
+// chrome.history.onVisited.addListener(function (historyItem) {
+//   // get the current time
+//   // let currentTime = Date.now();
+// //   console.log(historyItem.url);
+//   // console.log(JSON.stringify(historyItem))
+//   urls.push(historyItem.url);
+//   // if the tab is already in the map, update its active time
+
+//   chrome.runtime.sendMessage({ type: 'sendHistoryFromBackground', data: historyItem.url }, function (response) {
+//     // console.log(response.farewell);
+//   })
+
+// });
+
+
+
+
+
+///
+
+
+// chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+//     // console.log('message received in background......')
+//       if (request.type === 'getHistory') {
+//         // Get the user's history from the chrome.history API
+//         // console.log('get history');
+//         // console.log(urls)
+//         sendResponse({ history: urls});
+//       }
+//     });
+
+// const activeTab = {};
+// let activeTabNow
+
+// chrome.tabs.onActivated.addListener((activeInfo) => {
+//     chrome.tabs.get(activeInfo.tabId, (tab) => {
+//     activeTab.timestamp = Date.now();
+//     activeTab.url = tab.url;
+//     activeTabNow = tab
+//     console.log(activeTabNow.url + " " + tab.id + " active tab now" + activeTab.timestamp)
+//     });
+
+// });
     
+
+
+    
+// chrome.webNavigation.onCommitted.addListener(function(details) {
+//     // console.log("current url" + getActiveTabURL())
+//     var currentTab
+//     chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
+//         // The first element in the array (tabs[0]) represents the current tab
+//         currentTab = tabs[0].title;
+//         // console.log(currentTab.id);       // Outputs the ID of the current tab
+//         // currentTab = currentTab      // Outputs the URL of the current tab
+//         // console.log(currentTab.title);    // Outputs the title of the current tab
+//       });
+    
+//     console.log("URL: " + activeTabNow.url + "  ID: " + activeTabNow.id + " Transition type: " + details.transitionType + " URL: " + details.url);
+//     // console.log("Transition type: " + details.transitionType);
+//     // console.log("URL: " + details.url);
+//     // console.log("Details: " + JSON.stringify(details));
+    
+// });
+      
+// chrome.tabs.onRemoved.addListener(function(tabId, removeInfo) {
+//     console.log("Tab ID: " + tabId + " has been closed");
+//   });
+  
+
+
+// chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
+//     if (changeInfo.status === "loading") {
+//       if (tab.openerTabId) {
+//         console.log(tab.url + " navigated to from a previous page in a different tab")
+//       } else {
+//         console.log(tab.url + "The tab was navigated to from a previous page in the same tab")
+//         // The tab was navigated to from a previous page in the same tab
+//       }
+//     }
+//     if (changeInfo.url && changeInfo.status === 'complete') {
+//         console.log("Tab ID: " + tabId + " URL changed to: " + changeInfo.url);
+//       }
+//     chrome.runtime.sendMessage({tabId: tabId, changeInfo: changeInfo, tab: tab});
+//   });
+
+// //   chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
+// //     chrome.runtime.sendMessage({tabId: tabId, changeInfo: changeInfo, tab: tab});
+// //   });
+
+
+  
+  
+  
+
+// async function getActiveTabURL() {
+//     const tabs = await chrome.tabs.query({
+//         currentWindow: true,
+//         active: true
+//     });
+//     return tabs[0];
+//   }
--- src/newtab.js
+++ src/newtab.js
@@ -4,18 +4,268 @@ import Stats from 'three/addons/libs/stats.module.js';
 import { GUI } from 'three/addons/libs/lil-gui.module.min.js';
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 
-let camera, scene, renderer, controls, stats;
+
+
+chrome.storage.local.get('tabData', function(result) {
+
+  if(result.tabData){ 
+    presentTable(result.tabData);
+  }
+
+});
+
+
+
+chrome.storage.local.onChanged.addListener(function(changes, namespace) {
+  if(changes.tabData){
+    let tabData = changes.tabData.newValue;
+    presentTable(tabData);
+  }
+});
+
+
+function presentTable(tabData) {
+  var table = document.getElementById("tab-table");
+  var rowCount = table.rows.length;
+    for (var i = 1; i < rowCount; i++) {
+        table.deleteRow(1);
+    }
+  for (var tabId in tabData) {
+    var tab = tabData[tabId];
+    var row = table.insertRow();
+    var tabIdCell = row.insertCell();
+    var urlsCell = row.insertCell();
+    var activeCell = row.insertCell();
+    tabIdCell.innerHTML = tabId;
+    var urls = "";
+    for (var url in tab.url) {
+        urls += url + " navigated at " + tab.url[url] + "<br>";
+    }
+    urlsCell.innerHTML = urls;
+    var active = "";
+    for (var i = 0; i < tab.active.length; i++) {
+        active += tab.active[i] + "<br>";
+    }
+    activeCell.innerHTML = active;
+  }
+}
+
+
+
+let camera, scene, renderer, container, stats;
 
 async function getActiveTabURL() {
   const tabs = await chrome.tabs.query({
       currentWindow: true,
       active: true
   });
-
   return tabs[0];
 }
 
-// const iterateHistory = (history) => {
+
+
+			let mouseX = 0, mouseY = 0;
+
+			let windowHalfX = window.innerWidth / 2;
+			let windowHalfY = window.innerHeight / 2;
+
+			// init();
+			// animate();
+
+			function init() {
+
+				container = document.getElementById( 'container' );
+
+				camera = new THREE.PerspectiveCamera( 20, window.innerWidth / window.innerHeight, 1, 10000 );
+				camera.position.z = 500;
+
+        // const controls = new OrbitControls( camera, container );
+				// controls.minDistance = 0;
+				// controls.maxDistance = 100;
+
+				scene = new THREE.Scene();
+				scene.background = new THREE.Color( 0xffffff );
+
+				const light = new THREE.DirectionalLight( 0xffffff );
+				light.position.set( 0, 0, 1 );
+				scene.add( light );
+
+
+				renderer = new THREE.WebGLRenderer( { antialias: true } );
+				renderer.setPixelRatio( window.devicePixelRatio );
+				renderer.setSize( window.innerWidth, window.innerHeight );
+				container.appendChild( renderer.domElement );
+
+				stats = new Stats();
+				container.appendChild( stats.dom );
+
+				document.addEventListener( 'mousemove', onDocumentMouseMove );
+
+				window.addEventListener( 'resize', onWindowResize );
+
+
+        document.addEventListener("DOMContentLoaded", async () => {
+          const activeTab = await getActiveTabURL();
+          if (activeTab.url == "chrome://newtab/") {
+            console.log("new tab active ")
+          }
+          // go fetch new tab data from background script
+          chrome.runtime.sendMessage({ type: 'getHistory' }, function (response) {
+            console.log(response.history)
+            iterateHistorySpheres(response.history)
+          })
+        });
+    
+        // chrome.runtime.onMessage.addListener(
+        //   function(request, sender, sendResponse) {
+        //     // console.log(sender.tab ?
+        //     //             "from a content script:" + sender.tab.url :
+        //     //             "from the extension");
+        //     if (request.type == "sendHistoryFromBackground")
+        //       // console.log("in new tab")
+        //       console.log("from background " + request.data)
+        //       addSphere(request.data)
+        //       sendResponse({farewell: "goodbye"});
+        //   }
+        // );
+
+        function addSphere(_url) {
+          // console.log("adding sphere")
+          let sphereGeometry = new THREE.SphereGeometry(40, 40, 40);
+    
+    // // Create a sphere material
+    //       // let sphereMaterial = new THREE.MeshBasicMaterial({color: 0xffffff});
+          let color = new THREE.Color();
+          color.setHSL(Math.random(), Math.random(), Math.random());
+          let sphereMaterial = new THREE.MeshBasicMaterial({color: color});
+    
+    
+    // // Create a sphere mesh by combining the geometry and material
+          let sphere = new THREE.Mesh(sphereGeometry, sphereMaterial);
+    
+          // sphere.position.set(0, 0, 0);
+
+          sphere.position.x = Math.random() * 400 - 200;
+          sphere.position.y = Math.random() * 400 - 200;
+          sphere.position.z = Math.random() * 200 - 200;  
+    
+    // // Add the sphere to the scene
+          scene.add(sphere);
+
+        }
+    
+        function iterateHistorySpheres(history) {
+          // console.log("getting history in new tab")
+          console.log(history)
+          for( var i = scene.children.length - 1; i >= 0; i--) { 
+            obj = scene.children[i];
+            scene.remove(obj); 
+          }
+          for (const item in history) {
+            console.log(item + '' + history[item])
+            addSphere(history[item])
+          }
+        }
+			}
+
+			function onWindowResize() {
+
+				windowHalfX = window.innerWidth / 2;
+				windowHalfY = window.innerHeight / 2;
+
+				camera.aspect = window.innerWidth / window.innerHeight;
+				camera.updateProjectionMatrix();
+
+				renderer.setSize( window.innerWidth, window.innerHeight );
+
+			}
+
+			function onDocumentMouseMove( event ) {
+
+				mouseX = ( event.clientX - windowHalfX );
+				mouseY = ( event.clientY - windowHalfY );
+
+			}
+
+			//
+
+			function animate() {
+
+				requestAnimationFrame( animate );
+
+				render();
+				stats.update();
+
+			}
+
+			function render() {
+
+				// camera.position.x += ( mouseX - camera.position.x ) * 0.05;
+				// camera.position.y += ( - mouseY - camera.position.y ) * 0.05;
+
+				camera.lookAt( scene.position );
+
+				renderer.render( scene, camera );
+
+			}
+
+
+// var tabs = [];
+
+// iterateTabs()
+      
+
+
+// chrome.runtime.onMessage.addListener(
+//   function(request, sender, sendResponse) {
+//     if (request.type == "sendHistoryFromBackground")
+//       iterateTabs()
+//       // console.log("in new tab")
+//       // console.log(request.data)
+//       // iterateHistory(request.data)
+//       // sendResponse({farewell: "goodbye"});
+//   }
+// );
+
+
+// function iterateTabs() {
+//   chrome.tabs.query({}, function(tabs) {
+//     var tabList = document.getElementById("url_list");
+//     tabList.innerHTML = "";
+//     for (var i = 0; i < tabs.length; i++) {
+//       if (tabs.includes(tabs[i].id)) {
+//         continue
+//       }
+//       var row = document.createElement("tr");
+//       row.innerHTML = "<td>" + tabs[i].id + "</td>" +
+//                       "<td>" + tabs[i].url + "</td>" +
+//                       "<td>" + tabs[i].lastAccessed + "</td>";
+//       tabList.appendChild(row);
+//       tabs.push(tabs[i].id)
+//     }
+//   });
+// }
+
+
+      // chrome.runtime.onMessage.addListener(
+      //   function(request, sender, sendResponse) {
+      //     updateTabInfo(request.tabId, request.changeInfo, request.tab);
+      //   });
+      
+      // function updateTabInfo(tabId, changeInfo, tab) {
+      //   var tabList = document.getElementById("url_list");
+      //   for (var i = 0; i < tabList.rows.length; i++) {
+      //     var row = tabList.rows[i];
+      //     if (row.cells[0].innerHTML == tabId) {
+      //       row.cells[1].innerHTML = tab.url;
+      //       row.cells[2].innerHTML = tab.lastAccessed;
+      //       break;
+      //     }
+      //   }
+      // }
+      
+
+      // const iterateHistory = (history) => {
 //   console.log("getting history in new tab")
 //   var str = '<ul>'
 //   for (const item of history) {
@@ -42,242 +292,11 @@ async function getActiveTabURL() {
 //   })
 // });
 
-// chrome.runtime.onMessage.addListener(
-//   function(request, sender, sendResponse) {
-//     // console.log(sender.tab ?
-//     //             "from a content script:" + sender.tab.url :
-//     //             "from the extension");
-//     if (request.type == "sendHistoryFromBackground")
-//       console.log("in new tab")
-//       console.log(request.data)
-//       iterateHistory(request.data)
-//       sendResponse({farewell: "goodbye"});
-//   }
-// );
 
-  // function iterateHistorySpheres(history) {
-  //   console.log("getting history in new tab")
-  //   for (item in history) {
-  //     if (item.url in addedUrls) {
-  //       return;
-  //     }
-  //     addedUrls.push(item.url);
-  //     const geometry = new THREE.SphereGeometry( 22, 48, 24 );
-  //     const material = new THREE.MeshLambertMaterial( {
-  //       color: new THREE.Color().setHSL( Math.random(), 0.5, 0.5 ),
-  //       side: THREE.DoubleSide,
-  //       clippingPlanes: clipPlanes,
-  //       clipIntersection: params.clipIntersection
-  //     } );
-  //     group.add( new THREE.Mesh( geometry, material ) );
-  //   }
-  // }
 
 
 
-  let mesh;
-  // const amount = parseInt( window.location.search.slice( 1 ) ) || 10;
-  const amount = 1
-  const count = Math.pow( amount, 3 );
 
-  const raycaster = new THREE.Raycaster();
-  const mouse = new THREE.Vector2( 1, 1 );
 
-  const color = new THREE.Color();
-  const white = new THREE.Color().setHex( 0xffffff );
 
-  init();
-  animate();
 
-  function init() {
-
-    camera = new THREE.PerspectiveCamera( 60, window.innerWidth / window.innerHeight, 0.1, 100 );
-    camera.position.set( amount, amount, amount );
-    camera.lookAt( 0, 0, 0 );
-
-    scene = new THREE.Scene();
-
-    const light = new THREE.HemisphereLight( 0xffffff, 0x888888 );
-    light.position.set( 0, 1, 0 );
-    scene.add( light );
-
-    const geometry = new THREE.IcosahedronGeometry( 0.5, 3 );
-    const material = new THREE.MeshPhongMaterial( { color: 0xffffff } );
-
-
-
-
-    mesh = new THREE.InstancedMesh( geometry, material, count );
-
-    let i = 0;
-    const offset = ( amount - 1 ) / 2;
-
-    const matrix = new THREE.Matrix4();
-
-    for ( let x = 0; x < amount; x ++ ) {
-
-      for ( let y = 0; y < amount; y ++ ) {
-
-        for ( let z = 0; z < amount; z ++ ) {
-
-          matrix.setPosition( offset - x, offset - y, offset - z );
-
-          mesh.setMatrixAt( i, matrix );
-          mesh.setColorAt( i, color );
-
-          i ++;
-
-        }
-
-      }
-
-    }
-
-
-    document.addEventListener("DOMContentLoaded", async () => {
-      const activeTab = await getActiveTabURL();
-      if (activeTab.url == "chrome://newtab/") {
-        console.log("new tab active ")
-      }
-
-      // go fetch new tab data from background script
-      chrome.runtime.sendMessage({ type: 'getHistory' }, function (response) {
-        console.log(response.history)
-        iterateHistorySpheres(response.history)
-      })
-    });
-
-    chrome.runtime.onMessage.addListener(
-      function(request, sender, sendResponse) {
-        // console.log(sender.tab ?
-        //             "from a content script:" + sender.tab.url :
-        //             "from the extension");
-        if (request.type == "sendHistoryFromBackground")
-          console.log("in new tab")
-          console.log(request.data)
-          iterateHistorySpheres(request.data)
-          sendResponse({farewell: "goodbye"});
-      }
-    );
-
-    function iterateHistorySpheres(history) {
-      console.log("getting history in new tab")
-      console.log(history)
-      for (const item in history) {
-        console.log(item + '' + history[item])
-
-        // Create a sphere with radius 1
-      let sphereGeometry = new THREE.SphereGeometry(10, 10, 10);
-
-// Create a sphere material
-      // let sphereMaterial = new THREE.MeshBasicMaterial({color: 0xffffff});
-      let sphereMaterial = new THREE.MeshBasicMaterial({color: 0xff0000});
-
-
-// Create a sphere mesh by combining the geometry and material
-      let sphere = new THREE.Mesh(sphereGeometry, sphereMaterial);
-
-      sphere.position.set(0, 0, 0);
-
-// Add the sphere to the scene
-      scene.add(sphere);
-
-        // if (item.url in addedUrls) {
-        //   return;
-        // }
-        // addedUrls.push(item.url);
-        // const geometry = new THREE.SphereGeometry( 100, 100, 100 );
-        // const material = new THREE.MeshLambertMaterial( {
-        //   color: new THREE.Color().setHSL( Math.random(), 0.5, 0.5 ),
-        //   side: THREE.DoubleSide,
-        //   clippingPlanes: clipPlanes,
-        //   clipIntersection: params.clipIntersection
-        // } );
-        // scene.add( new THREE.Mesh( geometry, material ) );
-
-
-
-
-
-
-      }
-    }
-
-
-    const gui = new GUI();
-    gui.add( mesh, 'count', 0, count );
-
-    renderer = new THREE.WebGLRenderer( { antialias: true } );
-    renderer.setPixelRatio( window.devicePixelRatio );
-    renderer.setSize( window.innerWidth, window.innerHeight );
-    document.body.appendChild( renderer.domElement );
-
-    controls = new OrbitControls( camera, renderer.domElement );
-    controls.enableDamping = true;
-    controls.enableZoom = false;
-    controls.enablePan = false;
-
-    stats = new Stats();
-    document.body.appendChild( stats.dom );
-
-    window.addEventListener( 'resize', onWindowResize );
-    document.addEventListener( 'mousemove', onMouseMove );
-
-  }
-
-  function onWindowResize() {
-
-    camera.aspect = window.innerWidth / window.innerHeight;
-    camera.updateProjectionMatrix();
-
-    renderer.setSize( window.innerWidth, window.innerHeight );
-
-  }
-
-  function onMouseMove( event ) {
-
-    event.preventDefault();
-
-    mouse.x = ( event.clientX / window.innerWidth ) * 2 - 1;
-    mouse.y = - ( event.clientY / window.innerHeight ) * 2 + 1;
-
-  }
-
-  function animate() {
-
-    requestAnimationFrame( animate );
-
-    controls.update();
-
-    raycaster.setFromCamera( mouse, camera );
-
-    const intersection = raycaster.intersectObject( mesh );
-
-    if ( intersection.length > 0 ) {
-
-      const instanceId = intersection[ 0 ].instanceId;
-
-      mesh.getColorAt( instanceId, color );
-
-      if ( color.equals( white ) ) {
-
-        mesh.setColorAt( instanceId, color.setHex( Math.random() * 0xffffff ) );
-
-        mesh.instanceColor.needsUpdate = true;
-
-      }
-
-    }
-
-    render();
-
-    stats.update();
-
-  }
-
-  function render() {
-
-    renderer.render( scene, camera );
-
-  }
-   
